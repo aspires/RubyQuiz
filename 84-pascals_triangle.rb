2c65edcf12584e97d97cf2564de71eba718b8364
Austin Spires <austinspires@gmail.com>
2011-10-08 20:18:43 -0500

    MVP of pascal done

--- 84-pascals_triangle.rb
+++ 84-pascals_triangle.rb
@@ -5,22 +5,32 @@ rows = ARGV[0].to_i
 
 pascal = Array.new
 
-rows.times do |x|
-  pascal << Array.new
-end
+pascal << [1]
+rows = rows -1
+pascal << [1,1]
+rows = rows -1
+row_index = 1
 
-pascal.each do |x|
-  rows.times do x.push(" ")
-  end
-end
-
-pascal.map.with_index do |row, i|
-
-  leng = row.length
-  splits = i+2
-  inserts = leng/splits
+while rows > 0 
+  prevrow = pascal[row_index]
+  prevrow_length = prevrow.length
+  starting = 0
+  new_array = [1]
+  while starting < prevrow_length -1
+  next_place = starting+1
+  new_array << prevrow[starting].to_i + prevrow[next_place].to_i
   
-  puts inserts
-
+  starting = next_place
+  
+  end
+  
+  new_array << 1
+  
+  pascal << new_array
+  row_index +=1
+  
+  rows -= 1
+  
 end
+
 pp pascal
