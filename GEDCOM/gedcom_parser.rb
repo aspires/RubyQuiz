c964467245ec16db3e20b3b9c60ef7e86891c52a
Austin Spires <austinspires@gmail.com>
2011-11-01 22:41:36 -0500

    Working on GEDCOM

--- GEDCOM/gedcom_parser.rb
+++ GEDCOM/gedcom_parser.rb
@@ -1,7 +1,15 @@
-def parse_gedcom(line)
-  
-  return 42
-  
+
+gedcom_file = ARGV[0]
+
+def read_file(file)
+  File.read(file)
 end
 
+def parse_gedcom(line)
+  return 42
+end
 
+def split_gedcom(file_to_read)
+  #file_to_read.scan(/insert_regex_here/)
+  
+end
--- GEDCOM/test_gedcom_parser.rb
+++ GEDCOM/test_gedcom_parser.rb
@@ -1,10 +1,25 @@
 require "test/unit"
-
 require_relative "gedcom_parser"
 
+
+
 class TestGEDCOMParser < Test::Unit::TestCase
-  def test_returns_42
-    
-    assert_equal(43, parse_gedcom(" "))    
+  
+  def assign_raw_variables
+    file = "gedcom.txt"
+    blank_gedcom = read_file(file)
   end
+  
+  def test_returns_42
+    assert_equal(42, parse_gedcom(" "))    
+  end
+  
+  def test_file_read
+    assert_not_equal(nil, read_file(file), "File read in must not result in a nil")
+  end
+  
+  def test_file_split
+    assert_instance_of(Array, split_gedcom(blank_gedcom), "split_gedcom should return an array")
+  end
+  
 end
