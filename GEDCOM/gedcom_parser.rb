ff2e5e615522f409acb823cadd374ce35c6b91b6
Austin Spires <austinspires@gmail.com>
2011-11-13 22:32:34 -0600

    GEDCOM Parser work

--- GEDCOM/gedcom_parser.rb
+++ GEDCOM/gedcom_parser.rb
@@ -1,15 +1,36 @@
-
+require "pp"
 gedcom_file = ARGV[0]
+
+def test_gedcom(line)
+  return 42
+end
 
 def read_file(file)
   File.read(file)
 end
 
-def parse_gedcom(line)
-  return 42
+def read_gedcom(file_to_read)
+  file_to_read.slice(/^0 HEAD.*0 TRLR/m)
 end
 
-def split_gedcom(file_to_read)
-  #file_to_read.scan(/insert_regex_here/)
-  
+def parse_gedcom(gedcom_string)
+  new_array = gedcom_string.split(/^(0.*)/)
+  new_array.pop
+  2.times {new_array.delete_at(0)}
+  new_array
 end
+
+def turn_to_hash(input_array)
+  key = input_array.slice(0)
+  output_hash = Hash.new
+  hash_times = (input_array.length/2)
+  hash_times.times do 
+    loop_array = input_array.pop(2)
+    loop_hash = {loop_array[0]=>loop_array[1]}
+    output_hash.update(loop_hash)
+  end
+  [key, output_hash]
+end
+
+# pp a= turn_to_hash(parse_gedcom(read_gedcom(read_file(gedcom_file))))
+
--- GEDCOM/test_gedcom_parser.rb
+++ GEDCOM/test_gedcom_parser.rb
@@ -1,25 +1,56 @@
 require "test/unit"
 require_relative "gedcom_parser"
 
-
-
 class TestGEDCOMParser < Test::Unit::TestCase
   
-  def assign_raw_variables
-    file = "gedcom.txt"
-    blank_gedcom = read_file(file)
-  end
-  
   def test_returns_42
-    assert_equal(42, parse_gedcom(" "))    
+    assert_equal(42, test_gedcom(' '))
   end
   
   def test_file_read
-    assert_not_equal(nil, read_file(file), "File read in must not result in a nil")
+    assert_not_nil(read_file("gedcom.txt"), 
+                            "File read in must not result in a nil")
   end
   
-  def test_file_split
-    assert_instance_of(Array, split_gedcom(blank_gedcom), "split_gedcom should return an array")
+  def test_gedcom_read_to_s
+    assert_instance_of(String, read_gedcom(read_file("gedcom.txt")), 
+                      "read_gedcom should return an big string")
   end
   
+  def test_gedcom_parsing
+    assert_instance_of(Array, parse_gedcom(read_gedcom(read_file('gedcom.txt'))), 
+                      "parse_gedcom should spit out an array")
+  end
+  
+  def test_gedcom_array_cleaning
+    array = parse_gedcom(read_gedcom(read_file('gedcom.txt')))
+    assert_not_equal("0 TRLR", array[-1],
+                    "Parsed array should not include the footer")
+  end
+  
+  def test_array_front_cleaning_1of2
+    array = parse_gedcom(read_gedcom(read_file('gedcom.txt')))
+    assert_not_equal('' || "0 HEAD", array[0],
+                    "Array should have the header values removed"
+    )
+  end
+  
+  def test_array_front_cleaning_2of2
+    array = parse_gedcom(read_gedcom(read_file('gedcom.txt')))
+    assert_not_equal("0 HEAD", array[0],
+                    "Array should have the header values removed"
+    )
+  end
+  
+  def test_hash_splitter
+    array = parse_gedcom(read_gedcom(read_file('gedcom.txt')))
+    assert_instance_of(Array, turn_to_hash(array),
+                      "Should return an array")
+  end
+  
+  def test_did_it_make_a_hash
+    test_output = turn_to_hash(parse_gedcom(read_gedcom(read_file('gedcom.txt'))))
+    assert_instance_of(Hash, test_output[-1],
+                      "Should have a Hash in position 1")
+  end
 end
